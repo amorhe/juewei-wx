0a27ee8583fb9e0218ef05a429f8d356718b6e55
amorhe <2053734075@qq.com>
2019-09-02 19:32:12 +0800

    百度地图

--- pages/position/position.js
+++ pages/position/position.js
@@ -1,8 +1,27 @@
-import { imageUrl, ak, geotable_id } from '../common/js/baseUrl'
-import { bd_encrypt } from '../common/js/map'
-import { GetLbsShop, NearbyShop } from '../common/js/home'
-import { cur_dateTime, compare, sortNum } from '../common/js/time'
+import {
+  imageUrl,
+  ak,
+  geotable_id,
+  wxGet,
+  wxSet
+} from '../common/js/baseUrl'
+import {
+  bd_encrypt
+} from '../common/js/map'
+import {
+  GetLbsShop,
+  NearbyShop
+} from '../common/js/home'
+import {
+  cur_dateTime,
+  compare,
+  sortNum
+} from '../common/js/time'
 var app = getApp();
+var bmap = require('../../libs/bmap-wx.js');
+var BMap = new bmap.BMapWX({
+  ak: ak
+});
 Page({
 
   /**
@@ -16,41 +35,42 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    try {
-      wx.removeStorageSync('takeout');
-      wx.removeStorageSync('self');
-      wx.removeStorageSync('opencity');
-    } catch (e) {
-      // Do something when catch error
-    } 
+  onLoad: function(options) {
+    wx.removeStorageSync('takeout');
+    wx.removeStorageSync('self');
+    wx.removeStorageSync('opencity');
     var that = this;
     wx.getLocation({
       type: 'gcj02',
-      success(res) {
+      success(ott) {
         wx.hideLoading();
-        console.log(res);
         // 腾讯转百度
-        const mapPosition = bd_encrypt(res.longitude, res.latitude);
-        try {
-          wx.setStorageSync('lat', mapPosition.lat);
-          wx.setStorageSync('lng', mapPosition.lng);
-          // wx.setStorageSync('nearPois', res.pois);
-        } catch (e) {
-          // Do something when catch error
-        } 
+        // let mapPosition = bd_encrypt(ott.longitude, ott.latitude),
+         let res = {};
+        console.log(ott)
+        // 发起regeocoding检索请求 
+        BMap.regeocoding({
+          success(data) {
+            console.log(data)
+            res = data.originalData.result;
+            that.setData({
+              city: res.addressComponent.city
+            })
+            app.globalData.province = res.addressComponent.province;
+            app.globalData.city = res.addressComponent.city;
+            app.globalData.address = res.pois[0].name;
+            app.globalData.position = res;
+            app.globalData.position.longitude = res.location.lng;
+            app.globalData.position.latitude = res.location.lat;
+            wxSet('lat', res.location.lat);
+            wxSet('lng', res.location.lng);
+          }
+        });
         
-        // app.globalData.province = res.province;
-        // app.globalData.city = res.city;
-        // app.globalData.address = res.pois[0].name;
-        // app.globalData.position = res;
-        // app.globalData.position.longitude = mapPosition.bd_lng;
-        // app.globalData.position.latitude = mapPosition.bd_lat;
+
         // that.getLbsShop();
         // that.getNearbyShop();
-        // that.setData({
-        //   city: res.city
-        // })
+
       },
       fail() {
         // 定位失败
@@ -59,55 +79,55 @@ Page({
           url: '/pages/noposition/noposition'
         })
       },
-    })    
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
