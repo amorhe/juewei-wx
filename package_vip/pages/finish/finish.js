60e1d59612ca913c021534fc822b25754af9c9f6
bevexed <547551650@qq.com>
2019-09-12 16:11:10 +0800

    订单兑换完成页面

--- package_vip/pages/finish/finish.js
+++ package_vip/pages/finish/finish.js
@@ -1,17 +1,34 @@
 // package_vip/pages/finish/finish.js
+import { imageUrl, imageUrl2, baseUrl } from '../../../pages/common/js/baseUrl'
+import {  log, handleCopy, guide, contact, liTo } from '../../../pages/common/js/utils'
+import Request from "../../../pages/common/js/li-ajax";
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    imageUrl,
+    imageUrl2,
+    fail: false,
+    open1: false,
+    open2: false,
+    codeImg: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  async onLoad(query) {
+    let { id, fail } = query
+    this.setData({
+      fail: fail == 'true'
+    })
+    wx.setNavigationBarTitle({
+      title: fail != 'true' ? '兑换成功' : '兑换失败',
+    });
+    await this.getOdrderDetail(id)
 
   },
 
@@ -26,6 +43,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    this.closeModel()
 
   },
 
@@ -62,5 +80,129 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  redirect() {
+    wx.switchTab({
+      url: '/pages/vip/index/index', // 跳转的 tabBar 页面的路径（需在 app.json 的 tabBar 字段定义的页面）。注意：路径后不能带参数
+      success: (res) => {
+
+      },
+    });
+    // redirect('/pages/vip/index/index')
+  },
+
+  /**
+   * @function 跳转商品页
+   */
+
+  toOrderDetail(e) {
+    const { id } = e.currentTarget.dataset
+    wx.navigateTo({
+      url: '/package_vip/pages/exchangelist/exchangedetail/exchangedetail?id=' + id
+    });
+  },
+
+  /**
+   * 获取商品详情
+   */
+  async getOdrderDetail(id) {
+    let { code, data: { intro, exchange_intro, ...Data } } = await Request.reqOrderDetail({id});
+    if (code === 100) {
+      this.setData({
+        d: {
+          _intro: await parseData(intro),
+          _exchange_intro: await parseData(exchange_intro),
+          intro,
+          exchange_intro,
+          ...Data
+        }
+      })
+    }
+  },
+
+  /**
+   * @function 关闭弹窗
+   */
+
+  closeModel() {
+    this.setData({
+      open1: false,
+      open2: false
+    })
+  },
+
+  /**
+   * @function 使用优惠卷
+   */
+  async toUse() {
+    const { way } = this.data.d
+    // way:用途 1:外卖专享 2:门店专享 3:全场通用
+    switch (way - 0) {
+      case 1:
+      case 3:
+        this.setData({
+          open1: true
+        })
+        break;
+      case 2:
+        let { code } = this.data.d
+        let _sid = await getSid()
+        let codeImg = baseUrl + '/juewei-api/coupon/getQRcode?' + '_sid=' + _sid + '&code=' + code
+        log(codeImg)
+        this.setData({
+          open2: true,
+          codeImg
+        })
+        break
+    }
+  },
+
+
+
+  /**
+   * @function 去自提
+   */
+  toTakeOut() {
+    app.globalData.type = 2
+    log(app.globalData.type)
+    wx.switchTab({
+      url: '/pages/home/goodslist/goodslist'
+    });
+  },
+
+  /**
+   * @function 去外卖
+   */
+  toTakeIn() {
+    app.globalData.type = 1
+    log(app.globalData.type)
+
+    wx.switchTab({
+      url: '/pages/home/goodslist/goodslist'
+    });
+  },
+
+  /**
+   * @function 核销
+   */
+
+  async wait() {
+    let res = await reqWait()
+    if (res.code == 0) {
+      return this.closeModel()
+    }
+
+    return wx.showToast({
+      content: res.msg,
+    });
+  },
+
+  handleCopy,
+
+  guide,
+
+  contact,
+
+  liTo,
+
 })
