30cdf9b86ac1c699438219687641454a9f92c2a1
bevexed <547551650@qq.com>
2019-09-12 16:00:28 +0800

    VIP 详情页面

--- package_vip/pages/exchangelist/exchangedetail/exchangedetail.js
+++ package_vip/pages/exchangelist/exchangedetail/exchangedetail.js
@@ -1,10 +1,31 @@
 // package_vip/pages/exchangelist/exchangedetail/exchangedetail.js
+
+import { baseUrl, imageUrl, imageUrl2 } from '../../../../pages/common/js/baseUrl'
+import { log, handleCopy, guide, contact, event_getNavHeight } from '../../../../pages/common/js/utils'
+
 Page({
 
   /**
    * 页面的初始数据
    */
-  data: {
+   data: {
+    imageUrl,
+    imageUrl2,
+    fail: false,
+    open1: false,
+    open2: false,
+    cancleShow: false,
+
+
+    time: '',
+
+    cancelReasonList: [
+      { reason: '下错单/临时不想要了', value: true },
+      { reason: '信息填写错误，重新下单', value: false },
+      { reason: '其他', value: false },
+    ],
+    _exchange_intro: [],
+    _intro: [],
 
   },
 
