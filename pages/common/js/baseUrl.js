00f1d232b9f68c7698f0fefd83b3c496715bc8e7
JWD-PC\JWD <fuyu@juewei.com>
2019-10-08 15:06:22 +0800

    新增预发布环境

--- pages/common/js/baseUrl.js
+++ pages/common/js/baseUrl.js
@@ -13,6 +13,20 @@ export const geotable_id='134917';
 export const ak_wx = 'Xp8lScY0eQ50WN9dSyCmC3x058fU98O7';
 
 
+//预发布环境
+// export const baseUrl = 'https://proving-wap.juewei.com/api';
+// // 图片测试cdn
+// export const imageUrl = 'https://proving-cdn-wap.juewei.com/m/wx-mini/image/';
+// export const imageUrl2 = 'https://imgcdnjwd.juewei.com';
+// export const imageUrl3 = 'https://images.juewei.com';
+// //jsonUrl
+// export const jsonUrl = 'https://imgcdnjwd.juewei.com/static/product';
+// export const serviceUrl = 'https://proving-wap.juewei.com';
+// //百度测试ak
+// export const ak = 'pRtqXqnajTytAzWDL3HOnPRK';
+// export const geotable_id = '134917';
+// export const ak_wx = 'Xp8lScY0eQ50WN9dSyCmC3x058fU98O7';
+
 
 // // 生产环境
 // export const baseUrl = 'https://wap.juewei.com/api';
